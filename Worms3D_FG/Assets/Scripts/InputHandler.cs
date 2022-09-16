17c22ab13f897a19d65792e6fce20f2cae52bbf2
AinarScorp <Einarhallik@gmail.com>
2022-09-17 00:22:47 +0200

    h

--- Worms3D_FG/Assets/Scripts/InputHandler.cs
+++ Worms3D_FG/Assets/Scripts/InputHandler.cs
@@ -42,12 +42,12 @@ namespace Main.Inputs
 
         void Start()
         {
-            _playerInputs.Main.Jump.performed += ctx => _jumpInput = ctx.ReadValue<float>() > 0.1f ? true : false;
+            _playerInputs.Main.Jump.performed += ctx => _jumpInput = ctx.ReadValue<float>() > 0.1f;
             _playerInputs.Main.Move.performed += ctx => _movementInputs = ctx.ReadValue<Vector2>();
             _playerInputs.Main.Move.canceled += ctx => _movementInputs *= 0;
             _playerInputs.Main.Look.performed += ctx => _lookInput = ctx.ReadValue<Vector2>();
 
-            _playerInputs.Main.FirstPersonCamera.performed += ctx => _isAiming = ctx.ReadValue<float>() > 0.1f ? true : false;
+            _playerInputs.Main.FirstPersonCamera.performed += ctx => _isAiming = ctx.ReadValue<float>() > 0.1f;
         }
     }
 }
